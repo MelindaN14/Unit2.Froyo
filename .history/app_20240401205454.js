e36549f766f8384fc33ec07dfa5ce9839fad1d1d
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-04-01 21:06:08 -0400

    split

--- .history/app_20240401205454.js
+++ .history/app_20240401205454.js
@@ -1,6 +1,7 @@
 /* alert upon entering website asking user to enter a list of comma -sep froyo flavors
 they type : vanilla,vanilla,vanilla,strawberry,coffee,coffee
 */
-const userInput = prompt( "Please enter a list of comma separated froyo flavors");
-//array of flavors
-const flavors = userInput.split()
+const userInput = prompt(
+  "Please enter a list of comma separated froyo flavors"
+);
+//Split?
